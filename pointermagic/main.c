c26b90050df663390c1e703ed9e21944059a2d43
SigmaHyperon <nano_tech@web.de>
2017-03-27 13:05:13 +0200

    added pointermagic

---
+++ pointermagic/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int test[5] = {};
+	test[2] = 47;
+
+	float test2[5] = {};
+	test2[2] = 42;
+
+	printf("%i\n", test[2]);
+	printf("%i\n",2[test]);
+	printf("%f\n", test2[2]);
+	printf("%f\n",2[test2]);
+	return 0;
+}
